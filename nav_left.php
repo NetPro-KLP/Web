b6be35eacab5615b5691738e68af215da52c5d37
Park JungHyun <webdev.pjh@gmail.com>
2015-09-25 07:21:28 +0900

    nav_left split

---
+++ nav_left.php
@@ -0,0 +1,77 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+      <div class="sidebar-collapse">
+        <ul class="nav metismenu" id="side-menu">
+          <li class="nav-header">
+            <div class="dropdown profile-element"> 
+              <span>
+                <img alt="image" class="img-circle" src="https://d.hibiya.moe/OYN.png" />
+              </span>
+              <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+              <span class="clear">
+                <span class="block m-t-xs">
+                  <strong class="font-bold"><?=$result["name"]?></strong>
+                </span> 
+              <span class="text-muted text-xs block"><?=$result["position"]?></span> </span> </a>
+            </div>
+            <div class="logo-element">
+                NetPro-KLP
+            </div>
+          </li>
+          <li class="active">
+              <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboard</span></a>
+          </li>
+          <li>
+            <a href="metrics.html"><i class="fa fa-sitemap"></i> <span class="nav-label">User control</span>  </a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Packet Analysis</span><span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="graph_flot.html">Flot Charts</a></li>
+                <li><a href="graph_morris.html">Morris.js Charts</a></li>
+                <li><a href="graph_rickshaw.html">Rickshaw Charts</a></li>
+                <li><a href="graph_chartjs.html">Chart.js</a></li>
+                <li><a href="graph_chartist.html">Chartist</a></li>
+                <li><a href="graph_peity.html">Peity Charts</a></li>
+                <li><a href="graph_sparkline.html">Sparkline Charts</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="mailbox.html"><i class="fa fa-edit"></i> <span class="nav-label">Ruleset</span></a>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="mailbox.html">Inbox</a></li>
+                <li><a href="mail_detail.html">Email view</a></li>
+                <li><a href="mail_compose.html">Compose email</a></li>
+                <li><a href="email_template.html">Email templates</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="widgets.html"><i class="fa fa-globe"></i> <span class="nav-label">GeoIP</span></a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa fa-database"></i> 
+              <span class="nav-label">Log report</span>
+            </a>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="toastr_notifications.html">Notification</a></li>
+                <li><a href="nestable_list.html">Nestable list</a></li>
+                <li><a href="agile_board.html">Agile board</a></li>
+                <li><a href="timeline_2.html">Timeline v.2</a></li>
+                <li><a href="diff.html">Diff</a></li>
+                <li><a href="sweetalert.html">Sweet alert</a></li>
+                <li><a href="idle_timer.html">Idle timer</a></li>
+                <li><a href="spinners.html">Spinners</a></li>
+                <li><a href="tinycon.html">Live favicon</a></li>
+                <li><a href="google_maps.html">Google maps</a></li>
+                <li><a href="code_editor.html">Code editor</a></li>
+                <li><a href="modal_window.html">Modal window</a></li>
+                <li><a href="forum_main.html">Forum view</a></li>
+                <li><a href="validation.html">Validation</a></li>
+                <li><a href="tree_view.html">Tree view</a></li>
+                <li><a href="chat_view.html">Chat view</a></li>
+                <li><a href="masonry.html">Masonry</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
