f8ef1064d0d575d0e47a192418dbec53ba6503e9
Park JungHyun <webdev.pjh@gmail.com>
2015-10-07 13:51:38 +0900

    Common.js updated

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
 		var temp2 = $(temp[i]).attr("href");
 		if(temp2 == window.location.pathname ){
 			$("ul#side-menu > li a[href='" + temp2 + "']").parent("li").addClass("active");
+			$("ul#side-menu > li a[href='" + temp2 + "']").parent("li").parent("ul").removeClass("collapse");
+			$("ul#side-menu > li a[href='" + temp2 + "']").parent("li").parent("ul").addClass("collapse in");
+			$("ul#side-menu > li a[href='" + temp2 + "']").parent("li").parent("ul").parent("li").addClass("active");
 			break;
 		}
 	}
