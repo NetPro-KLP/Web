5055373344913e977df8059745e3bd87271cc924
Park JungHyun <webdev.pjh@gmail.com>
2015-10-07 13:49:39 +0900

    Packet Table php added

---
+++ PA/Packet-Table.php
@@ -0,0 +1,174 @@
+<?php include_once '../header.php'; ?>
+<link href="/assets/lib/css/plugins/footable/footable.core.css" rel="stylesheet">
+<body class="skin-1">
+  <div id="wrapper">
+    <?php include_once '../nav_left.php'; ?>
+    <div id="page-wrapper" class="gray-bg">
+      <?php include_once '../nav_top.php'; ?>
+      <div class="wrapper wrapper-content">
+	      <div class="row">
+		              <div class="col-lg-12">
+            <div class="ibox float-e-margins">
+		      <div class="ibox-title">
+											<h5>패킷 테이블</h5>
+                </div>
+		      <div class="ibox-content">
+<input type="text" class="form-control input-sm m-b-xs" id="filter"
+                           placeholder="찾기">
+                            <table class="footable table table-stripped toggle-arrow-tiny" data-page-size="8">
+                                <thead>
+                                <tr>
+                                    <th data-toggle="true">idx</th>
+                                    <th>닉네임</th>
+                                    <th>아이피</th>
+                                    <th>날짜</th>
+                                    <th data-hide="all">패킷</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Computer 1</td>
+                                    <td>127.0.0.1</td>
+                                    <td>2015-01-01 12:30:30</td>
+                                    <td>23:11:10.370344 IP (tos 0x20, ttl  64, id 35612, offset 0, flags [none], 
+length: 84) 72.21.34.42 > 69.254.213.43: icmp 64: echo reply seq 0</td>
+                                </tr>
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <td colspan="5">
+                                        <ul class="pagination pull-right"></ul>
+                                    </td>
+                                </tr>
+                                </tfoot>
+                            </table>
+
+                        </div>
+	      							</div>
+		            </div>
+	      </div>
+      </div>
+    </div>
+  </div>
+    <!-- Mainly scripts -->
+    <script src="/assets/lib/js/jquery-2.1.4.min.js"></script>
+    <script src="/assets/lib/js/bootstrap.min.js"></script>
+    <script src="/assets/lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="/assets/lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- FooTable -->
+    <script src="/assets/lib/js/plugins/footable/footable.all.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="/assets/lib/js/inspinia.js"></script>
+    <script src="/assets/lib/js/plugins/pace/pace.min.js"></script>
+    
+        <script>
+        $(document).ready(function() {
+
+            $('.footable').footable();
+            $('.footable2').footable();
+
+        });
+
+    </script>
+    
+  	<script src="/assets/js/common.js"></script>
+</body>
