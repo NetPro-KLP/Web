6f0ee2b8079efa6c960507a61a5c4fd90e579572
Park JungHyun <webdev.pjh@gmail.com>
2015-10-07 13:51:09 +0900

    Control Panel Added

---
+++ UC/Control-Panel.php
@@ -0,0 +1,53 @@
+<?php include_once '../header.php'; ?>
+<body class="skin-1">
+  <div id="wrapper">
+    <?php include_once '../nav_left.php'; ?>
+    <div id="page-wrapper" class="gray-bg">
+      <?php include_once '../nav_top.php'; ?>
+      <div class="wrapper wrapper-content">
+	      <div class="row">
+            <div class="col-lg-2">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+	                   	 <span class="badge badge-danger" style="position: absolute;top: -8px;left: 5px;">1</span>
+	                   	 <span class="badge badge-warning" style="position: absolute;top: -8px;left: 24px;">5</span>
+	                   	 <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="
+    position: absolute;
+    right: 10px;
+    top: -8px;
+">
+                                    <i class="fa fa-wrench"></i>
+                                </a>
+                        <span class="label label-success pull-right">정상</span>
+                        <h5>Computer 1</h5>
+                    </div>
+                    <div class="ibox-content">
+                        <h3 class="no-margins">127.0.0.1</h3>
+                        <p class="no-margins"><small>123,123,123KB</small></p>
+                        <p class="no-margins"><small>123,123,123KB</small></p>
+                        <span data-diameter="10" class="updating-chart">5,3,9,6,5,9,7,3,5,2,5,3,9,6,5,9,7,3,5,2</span>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+    <!-- Mainly scripts -->
+    <script src="/assets/lib/js/jquery-2.1.4.min.js"></script>
+    <script src="/assets/lib/js/bootstrap.min.js"></script>
+    <script src="/assets/lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="/assets/lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Peity -->
+    <script src="/assets/lib/js/plugins/peity/jquery.peity.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="/assets/lib/js/inspinia.js"></script>
+    <script src="/assets/lib/js/plugins/pace/pace.min.js"></script>
+		
+		<script src="/assets/js/Control-Panel.js"></script>
+		<script src="/assets/js/common.js"></script>
+</body>
