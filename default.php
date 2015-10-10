b42041fef8bb4a1467996603acf9ed64754ceffe
Park JungHyun <webdev.pjh@gmail.com>
2015-10-10 09:20:29 +0900

    default front page modified

--- default.php
+++ default.php
@@ -5,7 +5,19 @@
     <div id="page-wrapper" class="gray-bg">
       <?php include_once '../nav_top.php'; ?>
       <div class="wrapper wrapper-content">
+	      
       </div>
     </div>
   </div>
+    <!-- Mainly scripts -->
+    <script src="/assets/lib/js/jquery-2.1.4.min.js"></script>
+    <script src="/assets/lib/js/bootstrap.min.js"></script>
+    <script src="/assets/lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="/assets/lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="/assets/lib/js/inspinia.js"></script>
+    <script src="/assets/lib/js/plugins/pace/pace.min.js"></script>
+    
+  	<script src="/assets/js/common.js"></script>
 </body>
