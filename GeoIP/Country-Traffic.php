99d2bb57d472eff3ae89cf628936c08a708429db
Park JungHyun <webdev.pjh@gmail.com>
2015-10-10 09:06:12 +0900

    Country-Traffic Page added

--- GeoIP/Country-Traffic.php
+++ GeoIP/Country-Traffic.php
@@ -1,4 +1,5 @@
 <?php include_once '../header.php'; ?>
+<link href="/assets/css/Country-Traffic.css" rel="stylesheet">
 <body class="skin-1">
   <div id="wrapper">
     <?php include_once '../nav_left.php'; ?>
@@ -6,7 +7,7 @@
       <?php include_once '../nav_top.php'; ?>
       <div class="wrapper wrapper-content">
 	      <div class="row">
-              <div class="col-lg-7">
+              <div class="col-lg-12">
                   <div class="ibox float-e-margins">
                       <div class="ibox-title">
                           <h5>국가별 트래픽</h5>
@@ -15,10 +16,20 @@
 
                           <div class="row">
                               <div class="col-lg-12">
-                                  <div id="world-map"></div>
+                                  <div id="world-map" style="height:480px;"></div>
                               </div>
+                          </div>
                       </div>
-                      </div>
+                      <div class="ibox-footer">
+	                      <div class="size-group">
+		                      -
+	                        <span class="label label-danger">30,000MB 이상</span>
+	                        <span class="label label-warning">10,000MB 이상</span>
+	                        <span class="label label-success">100MB 이상</span>
+	                        <span class="label">0MB</span>
+	                        -
+	                      </div>
+	                    </div>
                   </div>
               </div>
           </div>
@@ -32,19 +43,6 @@
     <script src="/assets/lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="/assets/lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
-    <!-- Flot -->
-    <script src="/assets/lib/js/plugins/flot/jquery.flot.js"></script>
-    <script src="/assets/lib/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-    <script src="/assets/lib/js/plugins/flot/jquery.flot.spline.js"></script>
-    <script src="/assets/lib/js/plugins/flot/jquery.flot.resize.js"></script>
-    <script src="/assets/lib/js/plugins/flot/jquery.flot.pie.js"></script>
-    <script src="/assets/lib/js/plugins/flot/jquery.flot.symbol.js"></script>
-    <script src="/assets/lib/js/plugins/flot/jquery.flot.time.js"></script>
-
-    <!-- Peity -->
-    <script src="/assets/lib/js/plugins/peity/jquery.peity.min.js"></script>
-    <script src="/assets/lib/js/demo/peity-demo.js"></script>
-
     <!-- Custom and plugin javascript -->
     <script src="/assets/lib/js/inspinia.js"></script>
     <script src="/assets/lib/js/plugins/pace/pace.min.js"></script>
@@ -56,30 +54,40 @@
     <script src="/assets/lib/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
     <script src="/assets/lib/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 
-    <!-- EayPIE -->
-    <script src="/assets/lib/js/plugins/easypiechart/jquery.easypiechart.js"></script>
-
-    <!-- Sparkline -->
-    <script src="/assets/lib/js/plugins/sparkline/jquery.sparkline.min.js"></script>
-
-    <!-- Sparkline demo data  -->
-    <script src="/assets/lib/js/demo/sparkline-demo.js"></script>
-    
     <!-- Toastr -->
     <script src="/assets/lib/js/plugins/toastr/toastr.min.js"></script>
 
-    <script src="/assets/js/main.js"></script>
     <script>
-			    setTimeout(function() {
-		      toastr.options = {
-		          closeButton: true,
-		          progressBar: true,
-		          showMethod: 'slideDown',
-		          timeOut: 1500
-		      };
-		      toastr.success('<?=$result["name"]?> 서버 관리자님 환영합니다.', 'KLP-Firewall');
-		  }, 1300);
-	 	</script>
+	     var mapData = {
+        	"CN": 99
+    		};
+    $('#world-map').vectorMap({
+        map: 'world_mill_en',
+        backgroundColor: "transparent",
+        regionStyle: {
+            initial: {
+                fill: '#1ab394',
+                "fill-opacity": 0.9,
+                stroke: 'none',
+                "stroke-width": 0,
+                "stroke-opacity": 0
+            }
+        },
+        onRegionClick: function(event, code) {
+				  var map = $('#world-map').vectorMap('get', 'mapObject');
+				  mapData[code] = 120;
+				  map.series.regions[0].setValues(mapData);
+				},
+        series: {
+            regions: [{
+                values: mapData,
+                scale: ["#ff0000"],
+                normalizeFunction: 'polynomial'
+            }]
+        },
+    });
+	    </script>
+
 	 	
   	<script src="/assets/js/common.js"></script>
 </body>
