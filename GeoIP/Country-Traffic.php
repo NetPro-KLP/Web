12546667d8b3c990dc39e22f84e5419599e06b89
Park JungHyun <webdev.pjh@gmail.com>
2015-10-16 09:39:47 +0900

    Country Traffic block function added

--- GeoIP/Country-Traffic.php
+++ GeoIP/Country-Traffic.php
@@ -1,93 +1,55 @@
 <?php include_once '../header.php'; ?>
 <link href="/assets/css/Country-Traffic.css" rel="stylesheet">
 <body class="skin-1">
-  <div id="wrapper">
-    <?php include_once '../nav_left.php'; ?>
-    <div id="page-wrapper" class="gray-bg">
-      <?php include_once '../nav_top.php'; ?>
-      <div class="wrapper wrapper-content">
-	      <div class="row">
-              <div class="col-lg-12">
-                  <div class="ibox float-e-margins">
-                      <div class="ibox-title">
-                          <h5>국가별 트래픽</h5>
-                      </div>
-                      <div class="ibox-content">
-
-                          <div class="row">
-                              <div class="col-lg-12">
-                                  <div id="world-map" style="height:480px;"></div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="ibox-footer">
-	                      <div class="size-group">
-		                      -
-	                        <span class="label label-danger">30,000MB 이상</span>
-	                        <span class="label label-warning">10,000MB 이상</span>
-	                        <span class="label label-success">100MB 이상</span>
-	                        <span class="label">0MB</span>
-	                        -
-	                      </div>
-	                    </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      </div>
+    <div id="wrapper">
+        <?php include_once '../nav_left.php'; ?>
+        <div id="page-wrapper" class="gray-bg">
+            <?php include_once '../nav_top.php'; ?>
+            <div class="wrapper wrapper-content">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="ibox float-e-margins">
+                            <div class="ibox-title">
+                                <h5>국가별 트래픽</h5>
+                            </div>
+                            <div class="ibox-content">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div id="world-map" style="height:480px;"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="ibox-footer">
+                                <div class="size-group">
+                                    <span class="label">차단됨</span>
+                                    <span class="label label-danger">30,000MB 이상</span>
+                                    <span class="label label-warning">10,000MB 이상</span>
+                                    <span class="label label-success">100MB 이상</span>
+                                    <span class="label label-primary">0MB</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-   <!-- Mainly scripts -->
+    </div>
+    <!-- Mainly scripts -->
     <script src="/assets/lib/js/jquery-2.1.4.min.js"></script>
     <script src="/assets/lib/js/bootstrap.min.js"></script>
     <script src="/assets/lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="/assets/lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
     <!-- Custom and plugin javascript -->
     <script src="/assets/lib/js/inspinia.js"></script>
     <script src="/assets/lib/js/plugins/pace/pace.min.js"></script>
-
     <!-- jQuery UI -->
     <script src="/assets/lib/js/plugins/jquery-ui/jquery-ui.min.js"></script>
-
     <!-- Jvectormap -->
     <script src="/assets/lib/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
     <script src="/assets/lib/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-
     <!-- Toastr -->
     <script src="/assets/lib/js/plugins/toastr/toastr.min.js"></script>
-
-    <script>
-	     var mapData = {
-        	"CN": 99
-    		};
-    $('#world-map').vectorMap({
-        map: 'world_mill_en',
-        backgroundColor: "transparent",
-        regionStyle: {
-            initial: {
-                fill: '#1ab394',
-                "fill-opacity": 0.9,
-                stroke: 'none',
-                "stroke-width": 0,
-                "stroke-opacity": 0
-            }
-        },
-        onRegionClick: function(event, code) {
-				  var map = $('#world-map').vectorMap('get', 'mapObject');
-				  mapData[code] = 120;
-				  map.series.regions[0].setValues(mapData);
-				},
-        series: {
-            regions: [{
-                values: mapData,
-                scale: ["#ff0000"],
-                normalizeFunction: 'polynomial'
-            }]
-        },
-    });
-	    </script>
-
-	 	
-  	<script src="/assets/js/common.js"></script>
+    <script src="/assets/js/Country-Traffic.js"></script>
+    <script src="/assets/js/common.js"></script>
 </body>
