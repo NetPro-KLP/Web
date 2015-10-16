e3b33938296ccccebc5581eb7111ff22bd86c6ed
Park JungHyun <webdev.pjh@gmail.com>
2015-10-16 09:40:07 +0900

    Blacklist Paged added

---
+++ BL/Blacklist.php
@@ -0,0 +1,51 @@
+<?php include_once '../header.php'; ?>
+<link href="/assets/css/Country-Traffic.css" rel="stylesheet">
+<body class="skin-1">
+    <div id="wrapper">
+        <?php include_once '../nav_left.php'; ?>
+        <div id="page-wrapper" class="gray-bg">
+            <?php include_once '../nav_top.php'; ?>
+            <div class="wrapper wrapper-content">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="ibox float-e-margins">
+                            <div class="ibox-title">
+                                <h5>아이피 블랙리스트</h5>
+                            </div>
+                            <div class="ibox-content">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="ibox float-e-margins">
+                            <div class="ibox-title">
+                                <h5>국가 블랙리스트</h5>
+                            </div>
+                            <div class="ibox-content">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- Mainly scripts -->
+    <script src="/assets/lib/js/jquery-2.1.4.min.js"></script>
+    <script src="/assets/lib/js/bootstrap.min.js"></script>
+    <script src="/assets/lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="/assets/lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- Custom and plugin javascript -->
+    <script src="/assets/lib/js/inspinia.js"></script>
+    <script src="/assets/lib/js/plugins/pace/pace.min.js"></script>
+    <!-- jQuery UI -->
+    <script src="/assets/lib/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Jvectormap -->
+    <script src="/assets/lib/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="/assets/lib/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- Toastr -->
+    <script src="/assets/lib/js/plugins/toastr/toastr.min.js"></script>
+    <script src="/assets/js/common.js"></script>
+</body>
