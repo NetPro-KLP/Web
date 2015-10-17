e0e40fb78daf059852399ca0cd9493ac35dc1f0f
Park JungHyun <webdev.pjh@gmail.com>
2015-10-17 20:22:39 +0900

    Ruleset table ajax updated

--- RS/Ruleset-Table.php
+++ RS/Ruleset-Table.php
@@ -1,21 +1,22 @@
 <?php include_once '../header.php'; ?>
 <link href="/assets/lib/css/plugins/jqGrid/ui.jqgrid.css" rel="stylesheet">
 <body class="skin-1">
-  <div id="wrapper">
-    <?php include_once '../nav_left.php'; ?>
-    <div id="page-wrapper" class="gray-bg">
-      <?php include_once '../nav_top.php'; ?>
-       <div class="wrapper wrapper-content  animated fadeInRight">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="ibox ">
-                        <div class="ibox-title">
-                            <h5>룰셋 테이블</h5>
-                        </div>
-                        <div class="ibox-content">
-                            <div class="jqGrid_wrapper">
-                                <table id="table_list"></table>
-                                <div id="pager_list"></div>
+    <div id="wrapper">
+        <?php include_once '../nav_left.php'; ?>
+        <div id="page-wrapper" class="gray-bg">
+            <?php include_once '../nav_top.php'; ?>
+            <div class="wrapper wrapper-content  animated fadeInRight">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="ibox ">
+                            <div class="ibox-title">
+                                <h5>룰셋 테이블</h5>
+                            </div>
+                            <div class="ibox-content">
+                                <div class="jqGrid_wrapper">
+                                    <table id="table_list"></table>
+                                    <div id="pager_list"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -23,29 +24,22 @@
             </div>
         </div>
     </div>
-  </div>
-  
     <!-- Mainly scripts -->
     <script src="/assets/lib/js/jquery-2.1.4.min.js"></script>
     <script src="/assets/lib/js/bootstrap.min.js"></script>
     <script src="/assets/lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="/assets/lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
     <!-- Peity -->
     <script src="/assets/lib/js/plugins/peity/jquery.peity.min.js"></script>
-
     <!-- jqGrid -->
     <script src="/assets/lib/js/plugins/jqGrid/i18n/grid.locale-en.js"></script>
     <script src="/assets/lib/js/plugins/jqGrid/jquery.jqGrid.min.js"></script>
-    
     <!-- Custom and plugin javascript -->
     <script src="/assets/lib/js/inspinia.js"></script>
     <script src="/assets/lib/js/plugins/pace/pace.min.js"></script>
-   
-		<script src="/assets/js/Control-Panel.js"></script>
-		<script src="/assets/js/common.js"></script>
-		
-		<script>
+    <script src="/assets/js/Control-Panel.js"></script>
+    <script src="/assets/js/common.js"></script>
+    <script>
         $(document).ready(function () {
             $("#table_list").jqGrid({
                 url : "ajax.php",
@@ -57,7 +51,7 @@
                 rowNum: 25,
                 colNames:['idx','data'],
                 colModel:[
-	                	{name:'idx',index:'idx', width:5, align:"center", sorttype:"int",search:true},
+                 	{name:'idx',index:'idx', width:5, align:"center", sorttype:"int",search:true},
                     {name:'data',index:'data', width:90, editable: true, sorttype:"data",search:true}
                 ],
                 pager: "#pager_list",
@@ -67,43 +61,43 @@
                 edittext: 'Edit',
                 hidegrid: false,
                 sortname: "idx",
-								sortorder: "asc"
+        sortorder: "asc"
             });
             $("#table_list").jqGrid('navGrid', '#pager_list',
                 {edit: true, add: true, del: true, search: false},
                 {
                   url: "ajax.php",
                   mtype: "POST",
-			            reloadAfterSubmit: true,
-			            jqModal: true,
-			            closeOnEscape: true,
-			            closeAfterEdit: true,
-			            editData: {
-							        idx: function () {
-							            return $("#table_list").jqGrid('getCell', $("#table_list").jqGrid('getGridParam','selrow') , 'idx');
-							        }
-							    }
+               reloadAfterSubmit: true,
+               jqModal: true,
+               closeOnEscape: true,
+               closeAfterEdit: true,
+               editData: {
+               idx: function () {
+                   return $("#table_list").jqGrid('getCell', $("#table_list").jqGrid('getGridParam','selrow') , 'idx');
+               }
+           }
+                },
+                {
+                   url: "ajax.php",
+                   mtype: "POST",
+                   reloadAfterSubmit: true,
+                   jqModal: true,
+                   closeOnEscape: true,
+                   closeAfterEdit: true,
                 },
                 {
                   url: "ajax.php",
                   mtype: "POST",
-			            reloadAfterSubmit: true,
-			            jqModal: true,
-			            closeOnEscape: true,
-			            closeAfterEdit: true,
-                },
-                {
-                  url: "ajax.php",
-                  mtype: "POST",
-			            reloadAfterSubmit: true,
-			            jqModal: true,
-			            closeOnEscape: true,
-			            closeAfterEdit: true,
-			            delData: {
-							        idx: function () {
-							            return $("#table_list").jqGrid('getCell', $("#table_list").jqGrid('getGridParam','selrow') , 'idx');
-							        }
-							    }
+               reloadAfterSubmit: true,
+               jqModal: true,
+               closeOnEscape: true,
+               closeAfterEdit: true,
+               delData: {
+               idx: function () {
+                   return $("#table_list").jqGrid('getCell', $("#table_list").jqGrid('getGridParam','selrow') , 'idx');
+               }
+            }
                 }
             );
 
