f5712c3ef75637283e557450a421c15eafa635fb
Park JungHyun <webdev.pjh@gmail.com>
2015-10-17 20:22:52 +0900

    Ruleset ajax updated

--- RS/ajax.php
+++ RS/ajax.php
@@ -1,12 +1,12 @@
 <?php
 	session_start();
-	
+
 	if(empty($_SESSION["idx"]))
 		die();
-	
+
 	include_once '../db.php';
 	$db = new DB;
-	
+
 	$operation = $_POST['oper'];
 	$search = $_POST['search'];
 	$data = $_POST['data'];
@@ -15,7 +15,7 @@
 	$limit = $_POST['rows'];
 	$sidx = $_POST['sidx'];
 	$sord = $_POST['sord'];
-	
+
 	if($operation == "add")
 	{
 		$sql = "INSERT INTO `rules_data`(`data`) VALUES ('{$data}')";
@@ -40,16 +40,16 @@
 	{
 		if(!$sidx) $sidx = 1;
 		if(!$limit) $limit = 20;
-		
+
 		if($operation == "search")
 			$result = $db->mysqli->query("SELECT COUNT(*) AS count FROM rules_data where data=%'{$search}'%");
 		else
 			$result = $db->mysqli->query("SELECT COUNT(*) AS count FROM rules_data");
-			
+
 		$row = $result->fetch_array(MYSQL_ASSOC);
-		
+
 		$count = $row['count'];
-		
+
 		if( $count > 0 && $limit > 0) {
 			$total_pages = ceil($count/$limit);
 		} else {
@@ -58,19 +58,19 @@
 		if ($page > $total_pages) $page=$total_pages;
 		$start = $limit*$page - $limit;
 		if($start <0) $start = 0;
-		
+
 		if($operation == "search")
 			$SQL = "SELECT * FROM rules_data ORDER BY " . $sidx . " " . $sord . " LIMIT " . $start . " , " . $limit . "where data=%'" . $search . "'%";
 		else
 			$SQL = "SELECT * FROM rules_data ORDER BY " . $sidx . " " . $sord . " LIMIT " . $start . " , " . $limit . ";";
 		$result = $db->mysqli->query($SQL);
-		
+
 		$i=0;
-		
+
 		$responce->total = $total_pages;
 		while($row = $result->fetch_array(MYSQL_ASSOC)) {
 			$responce->rows[$i]['idx']=$row['idx'];
-			$responce->rows[$i]['data']=$row['data'];
+			$responce->rows[$i]['data']= htmlentities($row['data']);
 			$i++;
 		}
 		echo json_encode($responce);
