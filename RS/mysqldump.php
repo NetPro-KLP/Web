1446b705d61d8f3ec4066508cd86a9b2163e87bd
Park JungHyun <webdev.pjh@gmail.com>
2015-10-19 13:47:27 +0900

    git ignore added

--- RS/mysqldump.php
+++ RS/mysqldump.php
@@ -1,30 +1,30 @@
 <?php
 	include '../db.php';
 	$db = new DB;
-	
-	$DBUSER = "root";
-	$DBPASSWD = "klpsoma123";
-	$DATABASE = "KLP-Firewall";
+
+	$DBUSER = $db->getid();
+	$DBPASSWD = $db->getpw();
+	$DATABASE = $db->getdb();
 	$TABLE = "rules_data";
-	
+
 	$type = $_POST["type"];
-	
+
 	if($type == "backup")
 	{
-		$filename = "backup-" . date("Y-m-d h:m:s");
+		$filename = "backup-" . date('YmdHis');
 		$option = $_POST["option-backup"];
-		
+
 		if($option == "SQL")
 		{
 			$filename .= ".sql";
 			$mime = "application/octet-stream";
-			$cmd = "mysqldump -u $DBUSER --password=$DBPASSWD $DATABASE $TABLE"; 
+			$cmd = "mysqldump -u $DBUSER --password=$DBPASSWD $DATABASE $TABLE";
 		}
 		else if($option == "GZSQL")
 		{
 			$filename .= ".sql.gz";
 			$mime = "application/x-gzip";
-			$cmd = "mysqldump -u $DBUSER --password=$DBPASSWD $DATABASE $TABLE | gzip --best"; 
+			$cmd = "mysqldump -u $DBUSER --password=$DBPASSWD $DATABASE $TABLE | gzip --best";
 		}
 		else if($option == "CSV")
 		{
@@ -42,27 +42,27 @@
 			exec("mysql -u$DBUSER -p$DBPASSWD $DATABASE -e " . $fuckingsql);
 			$filepath = "/var/www/html/data/rules_data.rules";
 		}
-		
+
 		header( "Content-Type: " . $mime );
 		header( 'Content-Disposition: attachment; filename="' . $filename . '"' );
-		
+
 		if($option == "CSV" || $option == "RULES")
 			readfile($filepath);
 		else
 			passthru( $cmd );
-		
+
 		exit(0);
 	}
 	else if($type == "recovery")
 	{
 		// webshell upload protect
 		$filename = $_FILES["recovery-file"]["name"];
-		$exts = array('csv', 'sql', 'rules' , 'CSV' , 'SQL', 'RULES'); 
-		if(!in_array(end(explode('.', $filename)), $exts))
+		$exts = array('csv', 'sql', 'rules');
+		if(!in_array(strtolower(end(explode('.', $filename))), $exts))
 			exit();
-			
+
 		$option = $_POST["option-recovery"];
-		
+
 		if($option == "SQL")
 		{
 			move_uploaded_file($_FILES['recovery-file']['tmp_name'] , "/var/www/html/data/rules_data.sql");
@@ -80,11 +80,11 @@
 			move_uploaded_file($_FILES['recovery-file']['tmp_name'] , "/var/www/html/data/rules_data.rules");
 			$rl = fopen("/var/www/html/data/rules_data.rules", "r");
 			while (($line = fgets($rl)) !== false) {
-	      $sql = "INSERT INTO `rules_data`(`data`) VALUES ('{$line}')";
-	      $db->mysqli->query($sql);
-	    }
-	    fclose($rl);
-	    echo "<script>alert('적용 완료');window.location.href = '/RS';</script>";
+                $sql = "INSERT INTO `rules_data`(`data`) VALUES ('{$line}')";
+                $db->mysqli->query($sql);
+            }
+    	    fclose($rl);
+    	    echo "<script>alert('적용 완료');window.location.href = '/RS';</script>";
 		}
 	}
 ?>
