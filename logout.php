fa68a5a8984a6eb0de15a4f5f928c2b714994161
Park JungHyun <webdev.pjh@gmail.com>
2015-10-28 08:34:10 +0900

    jusuk removed

--- logout.php
+++ logout.php
@@ -1,18 +1,14 @@
 <?php
   session_start();
-  
-  // 모든 세션 변수 해제
+
   $_SESSION = array();
-  
-  // 세션을 없애려면, 세션 쿠키도 지웁니다.
-  // 주의: 이 동작은 세션 데이터뿐이 아닌, 세션 자체를 파괴합니다!
+
   if (isset($_COOKIE[session_name()])) {
       setcookie(session_name(), '', time()-42000, '/');
   }
-  
-  // 마지막으로, 세션 파괴.
+
   session_destroy();
-  
+
   header("Location: /");
   die();
 ?>
