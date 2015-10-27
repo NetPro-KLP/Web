2b869aaf2e300fbefe7eb0935e0967ed31107fba
Park JungHyun <webdev.pjh@gmail.com>
2015-10-28 08:36:23 +0900

    Control Panel backend

--- UC/Control-Panel.php
+++ UC/Control-Panel.php
@@ -1,53 +1,51 @@
 <?php include_once '../header.php'; ?>
+<link href="/assets/css/Control-Panel.css" rel="stylesheet">
 <body class="skin-1">
-  <div id="wrapper">
-    <?php include_once '../nav_left.php'; ?>
-    <div id="page-wrapper" class="gray-bg">
-      <?php include_once '../nav_top.php'; ?>
-      <div class="wrapper wrapper-content">
-	      <div class="row">
-            <div class="col-lg-2">
-                <div class="ibox float-e-margins">
-                    <div class="ibox-title">
-	                   	 <span class="badge badge-danger" style="position: absolute;top: -8px;left: 5px;">1</span>
-	                   	 <span class="badge badge-warning" style="position: absolute;top: -8px;left: 24px;">5</span>
-	                   	 <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="
-    position: absolute;
-    right: 10px;
-    top: -8px;
-">
-                                    <i class="fa fa-wrench"></i>
-                                </a>
-                        <span class="label label-success pull-right">정상</span>
-                        <h5>Computer 1</h5>
-                    </div>
-                    <div class="ibox-content">
-                        <h3 class="no-margins">127.0.0.1</h3>
-                        <p class="no-margins"><small>123,123,123KB</small></p>
-                        <p class="no-margins"><small>123,123,123KB</small></p>
-                        <span data-diameter="10" class="updating-chart">5,3,9,6,5,9,7,3,5,2,5,3,9,6,5,9,7,3,5,2</span>
-                        
-                    </div>
+    <div id="wrapper">
+        <?php include_once '../nav_left.php'; ?>
+        <div id="page-wrapper" class="gray-bg">
+            <?php include_once '../nav_top.php'; ?>
+            <div class="wrapper wrapper-content">
+                <div class="row">
+                    <?
+                        $result = $db->mysqli->query("SELECT users.idx, users.ip,users.connectedAt,users.status,SUM(packets.totalbytes) as traffic,SUM(packets.danger) as danger,SUM(packets.warn) as warn FROM users JOIN packets ON users.ip = packets.source_ip or users.ip = packets.destination_ip GROUP BY users.idx LIMIT 10");
+                        while($row = $result->fetch_array(MYSQL_ASSOC))
+                        {
+                            echo '<div class="col-xs-6 col-lg-2 user-box" id="' . $row["idx"] . '"><div class="ibox float-e-margins"><div class="ibox-title">';
+                            echo '<span class="badge badge-danger error-sign">' . $row["danger"] . '</span>';
+                            echo '<span class="badge badge-warning warn-sign">' . $row["warn"] . '</span>';
+                            echo '<a class="dropdown-toggle tool-set" data-toggle="dropdown" href="#"><i class="fa fa-wrench"></i></a>';
+                            echo '<ul class="dropdown-menu dropdown-user option">
+                                    <li><a class="show-packet" href="#">패킷보기</a></li>
+                                    <li><a class="block" href="#">차단하기</a></li>
+                                    <li><a class="remove" href="#">지우기</a></li>
+                                </ul>';
+                            if($row["status"] == 0)
+                                echo '<span class="label label-success pull-right">정상</span>';
+                            else
+                                echo '<span class="label label-danger pull-right">차단</span>';
+                            echo '<h5 class="ip">' . $row["ip"] . '</h5>';
+                            echo '<h7 class="date">' . $row["connectedAt"] . '</h7></div>';
+                            echo '<div class="ibox-content"><h5 class="traffic-span">Traffic</h5>';
+                            echo '<span><small>' . number_format($row["traffic"]) . '/kb</small></span>';
+                            echo '<span data-diameter="10" class="updating-chart" data-chart="' . $row["idx"] .'"></span>';
+                            echo '</div></div></div>';
+                        }
+                    ?>
                 </div>
             </div>
         </div>
-      </div>
     </div>
-  </div>
-  
     <!-- Mainly scripts -->
     <script src="/assets/lib/js/jquery-2.1.4.min.js"></script>
     <script src="/assets/lib/js/bootstrap.min.js"></script>
     <script src="/assets/lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="/assets/lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
     <!-- Peity -->
     <script src="/assets/lib/js/plugins/peity/jquery.peity.min.js"></script>
-
     <!-- Custom and plugin javascript -->
     <script src="/assets/lib/js/inspinia.js"></script>
     <script src="/assets/lib/js/plugins/pace/pace.min.js"></script>
-		
-		<script src="/assets/js/Control-Panel.js"></script>
-		<script src="/assets/js/common.js"></script>
+    <script src="/assets/js/Control-Panel.js"></script>
+    <script src="/assets/js/common.js"></script>
 </body>
