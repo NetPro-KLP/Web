7b230e7f93935d25e2ac963127b79006d1456c29
Park JungHyun <webdev.pjh@gmail.com>
2015-10-30 07:57:03 +0900

    auto backup daemon

---
+++ daemon.php
@@ -0,0 +1,23 @@
+<?
+    include_once 'db.php';
+
+    $db = new db();
+
+
+    for(;;)
+    {
+        $result = $db->mysqli->query("select remove_packet from system where 1");
+        $row = $result->fetch_array(MYSQL_ASSOC);
+        $sleep_count = $row['remove_packet'];
+
+        $move_data_sql = "INSERT INTO backup_packets SELECT * FROM packets";
+
+        $db->mysqli->query($move_data_sql);
+
+        $truncate_sql = "TRUNCATE table packets";
+
+        $db->mysqli->query($truncate_sql);
+
+        sleep($sleep_count);
+    }
+?>
