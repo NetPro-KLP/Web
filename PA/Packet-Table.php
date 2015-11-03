7137d0edf6b3915a386cbc313de55dd9ee7faff6
Park JungHyun <webdev.pjh@gmail.com>
2015-11-04 05:53:37 +0900

    Pagination Changed

--- PA/Packet-Table.php
+++ PA/Packet-Table.php
@@ -37,7 +37,7 @@
                                 <tfoot>
                                     <tr>
                                         <td colspan="11">
-                                            <div class="btn-group pull-right pagination">
+                                            <div class="btn-group pull-right pagination2">
 
                                             </div>
                                         </td>
@@ -62,55 +62,4 @@
 </script> <script src="/assets/lib/js/plugins/pace/pace.min.js">
 </script> <script src="/assets/js/common.js">
 </script>
-<script>
-    function getData(page){
-        $.ajax({
-            type: 'POST',
-            url: "./ajax.php",
-            data: "oper=table" + "&code=" + page,
-            success: function(res){
-                var spl = res.split("&");
-                var temp = spl[0];
-                temp = JSON.parse(temp);
-                Pagination(temp.total,page+1,10);
-                $("tbody").html(spl[1]);
-            },
-            error:function(err){
-                alert("데이터를 가져오는데 실패하였습니다.");
-            }
-        });
-    }
-    function Pagination(totalPages, nowPage, limit)
-    {
-    	$('.pagination').empty();
-    	$('.pagination').html('<button type="button" class="btn btn-white"><i class="fa fa-chevron-left"></i></button>');
-    	var currentPage = lowerLimit = upperLimit = Math.min(nowPage, totalPages);
-
-    	for (var b = 1; b < limit && b < totalPages;) {
-    	    if (lowerLimit > 1 ) { lowerLimit--; b++; }
-    	    if (b < limit && upperLimit < totalPages) { upperLimit++; b++; }
-    	}
-
-    	for (var i = lowerLimit; i <= upperLimit; i++) {
-    	    if (i == currentPage) $('.pagination').append('<button class="btn btn-white active">' + i + '</button>');
-    	    else $('.pagination').append('<button class="btn btn-white">' + i + '</button>');
-    	}
-    	$('.pagination').append('<button type="button" class="btn btn-white"><i class="fa fa-chevron-right"></i> </button>');
-
-    	$(".pagination > button").click(function(){
-    		var page = $(this).text();
-
-    		if(page == "PREV")
-    		{
-    			if(lowerLimit-1 < 1)
-    				page = 0;
-    			page = lowerLimit-1;
-    		}
-    		else if(page == "NEXT")
-    			page = upperLimit+1;
-            else
-    			getData(page-1);
-    	});
-    }
-    getData(0);
-</script>
+<script src="/assets/js/table.js"></script>
