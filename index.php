19df8911a1bb6a40c8ab764e0b0e478c7a02571f
Park JungHyun <webdev.pjh@gmail.com>
2015-11-11 14:19:19 +0900

    add tcp udp graph on socket.io

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
             <?
                 $sql1 = $db->mysqli->query("SELECT sum(packets.totalbytes) as sum FROM users JOIN packets ON users.ip = packets.destination_ip");
                 $sql2 = $db->mysqli->query("SELECT sum(packets.totalbytes) as sum FROM users JOIN packets ON users.ip = packets.source_ip");
-                $sql3 = $db->mysqli->query("SELECT count(idx) as count FROM packets");
+                $sql3 = $db->mysqli->query("SELECT sum(packet_count) as sum FROM `packets` WHERE 1");
                 $sql4 = $db->mysqli->query("SELECT count(idx) as count FROM users");
                 $sql5 = $db->mysqli->query("SELECT * from packet_log");
                 $row1 = mysqli_fetch_assoc($sql1);
@@ -21,7 +21,6 @@
                     <div class="col-lg-3">
                         <div class="ibox float-e-margins">
                             <div class="ibox-title">
-                                <span class="label label-primary pull-right">정상</span>
                                 <h5>인바운드</h5>
                             </div>
                             <div class="ibox-content">
@@ -33,7 +32,6 @@
                     <div class="col-lg-3">
                         <div class="ibox float-e-margins">
                             <div class="ibox-title">
-                                <span class="label label-danger pull-right">위험</span>
                                 <h5>아웃바운드</h5>
                             </div>
                             <div class="ibox-content">
@@ -45,11 +43,10 @@
                     <div class="col-lg-3">
                         <div class="ibox float-e-margins">
                             <div class="ibox-title">
-                                <span class="label label-warning pull-right">경고</span>
                                 <h5>패킷</h5>
                             </div>
                             <div class="ibox-content">
-                                <h1 class="no-margins"><?=number_format($row3['count'])?></h1>
+                                <h1 class="no-margins"><?=number_format($row3['sum'])?></h1>
                                 <small>개</small>
                             </div>
                         </div>
@@ -73,9 +70,9 @@
                                 <h5>트래픽 양</h5>
                                 <div class="pull-right">
                                     <div class="btn-group">
-                                        <button type="button" class="btn btn-xs btn-white active">어제</button>
-                                        <button type="button" class="btn btn-xs btn-white">오늘</button>
-                                        <button type="button" class="btn btn-xs btn-white">이번 달</button>
+                                        <button type="button" id="yesterday" class="btn btn-xs btn-white traffic">어제</button>
+                                        <button type="button" id="today" class="btn btn-xs btn-white traffic active">오늘</button>
+                                        <button type="button" id="month" class="btn btn-xs btn-white traffic">이번 달</button>
                                     </div>
                                 </div>
                             </div>
@@ -175,238 +172,6 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div id="right-sidebar">
-        <div class="sidebar-container">
-            <ul class="nav nav-tabs navs-3">
-                <li class="active"><a data-toggle="tab" href="#tab-1">
-                    Notes
-                    </a>
-                </li>
-                <li><a data-toggle="tab" href="#tab-2">
-                    Projects
-                    </a>
-                </li>
-                <li class=""><a data-toggle="tab" href="#tab-3">
-                    <i class="fa fa-gear"></i>
-                    </a>
-                </li>
-            </ul>
-            <div class="tab-content">
-                <div id="tab-1" class="tab-pane active">
-                    <div class="sidebar-title">
-                        <h3> <i class="fa fa-comments-o"></i> Latest Notes</h3>
-                        <small><i class="fa fa-tim"></i> You have 10 new message.</small>
-                    </div>
-                    <div>
-                        <div class="sidebar-message">
-                            <a href="#">
-                                <div class="pull-left text-center">
-                                    <img alt="image" class="img-circle message-avatar" src="img/a1.jpg">
-                                    <div class="m-t-xs">
-                                        <i class="fa fa-star text-warning"></i>
-                                        <i class="fa fa-star text-warning"></i>
-                                    </div>
-                                </div>
-                                <div class="media-body">
-                                    There are many variations of passages of Lorem Ipsum available.
-                                    <br>
-                                    <small class="text-muted">Today 4:21 pm</small>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="sidebar-message">
-                            <a href="#">
-                                <div class="pull-left text-center">
-                                    <img alt="image" class="img-circle message-avatar" src="img/a2.jpg">
-                                </div>
-                                <div class="media-body">
-                                    The point of using Lorem Ipsum is that it has a more-or-less normal.
-                                    <br>
-                                    <small class="text-muted">Yesterday 2:45 pm</small>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="sidebar-message">
-                            <a href="#">
-                                <div class="pull-left text-center">
-                                    <img alt="image" class="img-circle message-avatar" src="img/a3.jpg">
-                                    <div class="m-t-xs">
-                                        <i class="fa fa-star text-warning"></i>
-                                        <i class="fa fa-star text-warning"></i>
-                                        <i class="fa fa-star text-warning"></i>
-                                    </div>
-                                </div>
-                                <div class="media-body">
-                                    Mevolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                                    <br>
-                                    <small class="text-muted">Yesterday 1:10 pm</small>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="sidebar-message">
-                            <a href="#">
-                                <div class="pull-left text-center">
-                                    <img alt="image" class="img-circle message-avatar" src="img/a4.jpg">
-                                </div>
-                                <div class="media-body">
-                                    Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the
-                                    <br>
-                                    <small class="text-muted">Monday 8:37 pm</small>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="sidebar-message">
-                            <a href="#">
-                                <div class="pull-left text-center">
-                                    <img alt="image" class="img-circle message-avatar" src="img/a8.jpg">
-                                </div>
-                                <div class="media-body">
-                                    All the Lorem Ipsum generators on the Internet tend to repeat.
-                                    <br>
-                                    <small class="text-muted">Today 4:21 pm</small>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="sidebar-message">
-                            <a href="#">
-                                <div class="pull-left text-center">
-                                    <img alt="image" class="img-circle message-avatar" src="img/a7.jpg">
-                                </div>
-                                <div class="media-body">
-                                    Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                                    <br>
-                                    <small class="text-muted">Yesterday 2:45 pm</small>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="sidebar-message">
-                            <a href="#">
-                                <div class="pull-left text-center">
-                                    <img alt="image" class="img-circle message-avatar" src="img/a3.jpg">
-                                    <div class="m-t-xs">
-                                        <i class="fa fa-star text-warning"></i>
-                                        <i class="fa fa-star text-warning"></i>
-                                        <i class="fa fa-star text-warning"></i>
-                                    </div>
-                                </div>
-                                <div class="media-body">
-                                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below.
-                                    <br>
-                                    <small class="text-muted">Yesterday 1:10 pm</small>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="sidebar-message">
-                            <a href="#">
-                                <div class="pull-left text-center">
-                                    <img alt="image" class="img-circle message-avatar" src="img/a4.jpg">
-                                </div>
-                                <div class="media-body">
-                                    Uncover many web sites still in their infancy. Various versions have.
-                                    <br>
-                                    <small class="text-muted">Monday 8:37 pm</small>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div id="tab-2" class="tab-pane">
-                    <div class="sidebar-title">
-                        <h3> <i class="fa fa-cube"></i> Latest projects</h3>
-                        <small><i class="fa fa-tim"></i> You have 14 projects. 10 not completed.</small>
-                    </div>
-                    <ul class="sidebar-list">
-                        <li>
-                            <a href="#">
-                                <div class="small pull-right m-t-xs">9 hours ago</div>
-                                <h4>Business valuation</h4>
-                                It is a long established fact that a reader will be distracted.
-                                <div class="small">Completion with: 22%</div>
-                                <div class="progress progress-mini">
-                                    <div style="width: 22%;" class="progress-bar progress-bar-warning"></div>
-                                </div>
-                                <div class="small text-muted m-t-xs">Project end: 4:00 pm - 12.06.2014</div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <div class="small pull-right m-t-xs">9 hours ago</div>
-                                <h4>Contract with Company </h4>
-                                Many desktop publishing packages and web page editors.
-                                <div class="small">Completion with: 48%</div>
-                                <div class="progress progress-mini">
-                                    <div style="width: 48%;" class="progress-bar"></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <div class="small pull-right m-t-xs">9 hours ago</div>
-                                <h4>Meeting</h4>
-                                By the readable content of a page when looking at its layout.
-                                <div class="small">Completion with: 14%</div>
-                                <div class="progress progress-mini">
-                                    <div style="width: 14%;" class="progress-bar progress-bar-info"></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="label label-primary pull-right">NEW</span>
-                                <h4>The generated</h4>
-                                There are many variations of passages of Lorem Ipsum available.
-                                <div class="small">Completion with: 22%</div>
-                                <div class="small text-muted m-t-xs">Project end: 4:00 pm - 12.06.2014</div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <div class="small pull-right m-t-xs">9 hours ago</div>
-                                <h4>Business valuation</h4>
-                                It is a long established fact that a reader will be distracted.
-                                <div class="small">Completion with: 22%</div>
-                                <div class="progress progress-mini">
-                                    <div style="width: 22%;" class="progress-bar progress-bar-warning"></div>
-                                </div>
-                                <div class="small text-muted m-t-xs">Project end: 4:00 pm - 12.06.2014</div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <div class="small pull-right m-t-xs">9 hours ago</div>
-                                <h4>Contract with Company </h4>
-                                Many desktop publishing packages and web page editors.
-                                <div class="small">Completion with: 48%</div>
-                                <div class="progress progress-mini">
-                                    <div style="width: 48%;" class="progress-bar"></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <div class="small pull-right m-t-xs">9 hours ago</div>
-                                <h4>Meeting</h4>
-                                By the readable content of a page when looking at its layout.
-                                <div class="small">Completion with: 14%</div>
-                                <div class="progress progress-mini">
-                                    <div style="width: 14%;" class="progress-bar progress-bar-info"></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="label label-primary pull-right">NEW</span>
-                                <h4>The generated</h4>
-                                There are many variations of passages of Lorem Ipsum available.
-                                <div class="small">Completion with: 22%</div>
-                                <div class="small text-muted m-t-xs">Project end: 4:00 pm - 12.06.2014</div>
-                            </a>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>
@@ -558,7 +323,7 @@
             },
             onRegionTipShow: function(e, el, code){
                 if(mapData[code])
-                    el.html(el.html() + "<br>Traffic - " + mapData[code].toLocaleString() + "KB");
+                    el.html(el.html() + "<br>Traffic - " + (mapData[code]/1024).toLocaleString() + "MB");
             }
         });
     </script>
